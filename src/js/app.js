b2c47713ffad0f14e38e4f62cce6dea4c4a4ded7
Yash Parihar <pariharyash6@gmail.com>
2018-04-16 19:39:11 +0530

    loading multiple html file, jquery lib

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,23 @@
-
+import 'jquery';
 import '../css/main.scss';
+import '../users.html';
+
 import { RandomGenerator } from './random-generator.js';
 
 
-const outputParagraph = document.querySelector('#outputParagraph');
+const outputParagraph = $('#outputParagraph');
 
 const outputRandomInt = () => {
-    outputParagraph.textContent = RandomGenerator.randomInteger();
+    outputParagraph.text(RandomGenerator.randomInteger());
 };
 
 const outputRandomRange = () => {
-    outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
+    outputParagraph.text(RandomGenerator.randomRange(1, 500));
 };
 
-const buttonRndInt = document.querySelector('#randomInt');
+const buttonRndInt = jQuery('#randomInt');
 const buttonRndRange = document.querySelector('#randomRange');
 
-buttonRndInt.addEventListener('click', outputRandomInt);
+buttonRndInt.click(outputRandomInt);
+
 buttonRndRange.addEventListener('click', outputRandomRange);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 var path = require('path');
+var webpack = require('webpack');
 var extractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new extractTextPlugin({
     filename: 'main.css'
 });
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: {
+        appa: './src/js/app.js',
+    },
     output: {
         path : path.resolve(__dirname , 'dt'),
         filename: 'bundle.js',
-        publicPath: "/dt"
+       // publicPath: "/dt"
     },
     module: {
         rules : [
@@ -30,10 +35,56 @@ module.exports = {
                 use : extractPlugin.extract({
                     use : ['css-loader' , 'sass-loader']
                 })
+            },
+
+            {
+                test: /\.html$/,
+                use: [ 'html-loader']
+            },
+            {
+                test:  /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test:  /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname,'src/index.html')
             }
+
+
         ]
     },
     plugins: [
-        extractPlugin
+        new webpack.ProvidePlugin({
+            $ : 'jquery',
+            jQuery: 'jquery'
+        }),
+        extractPlugin,
+        // new HtmlWebpackPlugin({
+        //     filename: 'users.html',
+        //     template: 'src/users.html',
+        //     chunks:['appb']
+        // }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dt'])
     ]
 }
